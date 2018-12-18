519f3c76734b2ba5e31fa9f12742335cc904cd9c
robj137 <robj137@gmail.com>
2018-12-18 01:52:17 -0800

    working solution for day 17 (~ 10 seconds)

--- day17.py
+++ day17.py
@@ -31,10 +31,9 @@ def main():
   xmax, ymax = np.max(clay, axis=0)
   print(ymin, ymax, xmin, xmax)
 
-  clay_map = np.zeros([ymax+5, xmax-xmin + 3])
-  offset = xmin-1
-  water_source = (0,500-offset)
-  clay_map[water_source] = 9
+  clay_map = np.zeros([ymax+5, xmax-xmin + 5])
+  offset = xmin
+  clay_map[(ymin, 500-offset)] = 9
   
   for el in clay:    
     y, x = el
@@ -42,11 +41,93 @@ def main():
   
   print(clay_map)
  
-  sources = [water_source]
-  basins = []
+  vertical_step(clay_map, (ymin, 500-offset))
+  print('\n')
+  print(clay_map)
 
+  n_tiles_flowing = np.argwhere(clay_map == 2).shape[0]
+  n_tiles_standing = np.argwhere(clay_map == 3).shape[0]
+  
+  print(n_tiles_flowing, n_tiles_standing, n_tiles_flowing + n_tiles_standing)
+  print('Part a: All \'wet\' tiles (standing + flowing) = {}'.format(n_tiles_flowing + n_tiles_standing))
+  print('Part b: All standing water tiles: {}'.format(n_tiles_standing))
+  #with open('view.txt', 'w') as f:
+  #  for line in clay_map:
+  #    f.write(''.join([str(int(x)) for x in line]) + '\n')
+
+
+
+def horizontal_step(m, pos):
+  # below was clay or water
+  # want to crawl left and right until we come to either
+  # a: a clay wall
+  # b: a drop (below = 0)
+  # should mark as flowing water (2) as cral
+  # if we find we're in a basin, turn all the flowing water to standing water (3)
+  # eithe way, return the type of origin (2 or 3)
+  visited = []
+  current = pos[:]
+  borders = 0
+  while 1:
+    # go left
+    visited.append(current)
+    m[current] = 2 # flowing water
+    nxt_down = tuple(np.array(current) + (1, 0))
+
+    if m[nxt_down] == 0:
+      # down we go again!
+      # nothing more to do except call the vertical step and then break
+      down = vertical_step(m, nxt_down)
+      if down == 2:
+        break
+    nxt_pos = tuple(np.array(current) + (0,-1))
+    if m[nxt_pos] == 1:
+      borders += 1
+      break
+    current = nxt_pos
+  while 1:
+    visited.append(current)
+    m[current] = 2 # flowing water
+    nxt_down = tuple(np.array(current) + (1, 0))
+
+    if m[nxt_down] == 0:
+      # down we go again!
+      # nothing more to do except call the vertical step and then break
+      down = vertical_step(m, nxt_down)
+      if down == 2:
+        break
+    # go right
+    nxt_pos = tuple(np.array(current) + (0,1))
+    if m[nxt_pos] == 1:
+      borders += 1
+      break
+    current = nxt_pos
+  if borders == 2:
+    #it's actually standing water!
+    for p in visited:
+      m[p] = 3 # standing water
+    return 3
+  return 2
+
+def vertical_step(m, pos):
+  # possible node types:
+  # just vertical water, only |
+  # horizontal push: below is 
+  if pos[0] > np.max(np.argwhere(m > 0)[:,0]):
+    return 2 #out of bounds, so we don't change the map for this one
+  m[pos] = 2 # flowing water
+  below_ndx = tuple(np.array(pos) + (1,0))
+  below = m[below_ndx]
+  if below == 0:
+    # perform the vertical step. below might get changed to standing water....
+    below = vertical_step(m, below_ndx)
+  if below == 1 or below == 3:
+    # now we need to do the horizontal step
+    this_type = horizontal_step(m, pos)
+  return m[pos] # this will get updated to standing water if necessary in the horizontal step
 
 if __name__ == '__main__':
+  # 338 too low
   begin = dt.datetime.now()
   main()
   diff_time = dt.datetime.now() - begin
